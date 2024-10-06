1890bce0996f6bc21c19908250ad29c5b13440d4
josue <josue@Ubuntu.myguest.virtualbox.org>
2024-10-06 15:47:10 -0300

    Adicionado o comando para requerer a key master no ambiente de produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,9 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
 
-  # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
-  # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
+   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
+   config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
