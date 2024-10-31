ae098a52ffe4115b309cd25bcca0d5d9eb812978
josue <josue@Ubuntu.myguest.virtualbox.org>
2024-10-30 22:46:52 -0300

    Adicionado CRUD para o recurso Phone

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -46,6 +46,7 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate, :kind_id)
+      params.require(:contact).permit(:name, :email, :birthdate, :kind_id,
+      phones_attributes: %i[ id number _destroy])
     end
 end
