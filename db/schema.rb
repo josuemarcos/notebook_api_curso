05d05be49ea09850417a41847e3463a89496bfc9
josue <josue@Ubuntu.myguest.virtualbox.org>
2024-11-06 12:38:08 -0300

    Adicionados serializers de phones e address; Feita restrição para tipo json na requisição

--- db/schema.rb
+++ db/schema.rb
@@ -20,15 +20,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_190612) do
     t.index ["contact_id"], name: "index_addresses_on_contact_id"
   end
 
-  create_table "adresses", force: :cascade do |t|
-    t.string "street"
-    t.string "city"
-    t.integer "contact_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["contact_id"], name: "index_adresses_on_contact_id"
-  end
-
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -54,7 +45,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_190612) do
   end
 
   add_foreign_key "addresses", "contacts"
-  add_foreign_key "adresses", "contacts"
   add_foreign_key "contacts", "kinds"
   add_foreign_key "phones", "contacts"
 end
