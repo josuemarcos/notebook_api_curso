52e6c88e0fdd1f6fa957a5e3036123b18ae40d20
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2024-12-03 19:06:04 +0000

    Remove duplicação de configuração da porta no Puma

--- config/puma.rb
+++ config/puma.rb
@@ -19,7 +19,7 @@ on_worker_boot do
 end
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+#port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
